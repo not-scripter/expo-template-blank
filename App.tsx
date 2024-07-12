eb9381e6868f5958305a23cc24e77e5a12c55705
MRINMOY MONDAL <mm2922894@gmail.com>
2024-07-13 02:34:15 +0530

    up

--- App.tsx
+++ App.tsx
@@ -1,16 +1,13 @@
-import { NativeWindStyleSheet } from "nativewind";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center dark:bg-black">
-      <Text className="text-2xl font-bold dark:text-white">Namaste World!</Text>
+      <Text className="text-2xl font-bold dark:text-white/80">
+        Namaste World!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-NativeWindStyleSheet.setOutput({
-  default: "native",
-});
